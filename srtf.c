b2adb4208a1e9f268742091f52f7f18a3cbb3122
JacRay <64971844+JacRay@users.noreply.github.com>
2021-10-04 14:44:41 +0530

    Srtf

---
+++ srtf.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+srtf(){
+
+}
+void Input(int ch){
+	printf("Enter No of Job:");
+	scanf("%i",&r);
+	for(int i=0;i<r;i++)
+	{
+		printf("\n Enter Burst Time:");
+		scanf("%i",&burst[i]);
+		printf("\n Enter Arrival Time:");
+		scanf("%i",&arrival[i]);
+    }
+		srtf();
+		display(2);
+}
+int main(){
+    int burst[20],arrival[20],wait[20],turnaround[20],r;
+    printf("\n Enter the No of Process: ");
+    scanf("%i", &r);
+    printf("\n Enter the No of: ");
+
+
+}
