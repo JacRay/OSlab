825b631eda83324e58d6ed81f26c9d6ed9322450
JacRay <64971844+JacRay@users.noreply.github.com>
2021-11-08 22:22:16 +0530

    Average sellig price

--- Average Buying Price.py
+++ Average Buying Price.py
@@ -1,7 +1,12 @@
 r = int(input("\n Enter No of buys:"))
 price = []
 q = []
+sum = 0
+sumq = 0
 for x in range(r):
     price.append(input("\n Enter Price:"))
     q.append(input("\n Enter Quantity:"))
-
+    sum += int(price[x])*int(q[x])
+    sumq += int(q[x])
+print("\n Average price:", sum/sumq)
+print("\n Quantity:", sumq)
